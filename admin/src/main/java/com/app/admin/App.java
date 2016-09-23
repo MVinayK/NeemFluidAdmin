561629f2e0173dec5c20b24dbf3b3d97b7d3d862
vinay <muppanavinay@gmail.com>
2016-09-24 01:05:27 +0530

    neemfluid first commit

---
+++ admin/src/main/java/com/app/admin/App.java
@@ -0,0 +1,30 @@
+package com.app.admin;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.servlet.ServletContainer;
+
+public class App {
+
+	public static void main(String[] args) {
+		ResourceConfig config = new ResourceConfig();
+		config
+		 	.packages("com.service.admin");
+		ServletHolder servlet = new ServletHolder(new ServletContainer(config));
+
+		Server server = new Server(8181);
+		ServletContextHandler context = new ServletContextHandler(server, "/*");
+		context.addServlet(servlet, "/*");
+		try {
+		     server.start();
+		     server.join();
+		 } catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+		     server.destroy();
+		}
+	}
+
+}
